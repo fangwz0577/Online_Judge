f33259d02db301d2e3469311db33eb00a56f7c9c
fwz <fwz@localhost.localdomain>
2014-11-07 15:51:34 +0800

    change dirs

---
+++ hdoj/1000.cpp
@@ -0,0 +1,28 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  1000.cpp
+ *
+ *    Description:  A + B problem
+ *
+ *        Version:  1.0
+ *        Created:  11/06/2014 10:19:15 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Fang Wenzhou (), fwenzhou@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    while(cin>>a>>b)
+        cout<<a+b<<endl;
+    return 0;
+}
